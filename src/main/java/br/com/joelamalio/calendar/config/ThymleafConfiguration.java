abcc3c6794a97f125adb4f5dd690a0d5e936d931
Joel Amálio <joel.amalio@gmail.com>
2018-11-05 23:11:08 -0200

    Configuração inicial do template.

---
+++ src/main/java/br/com/joelamalio/calendar/config/ThymleafConfiguration.java
@@ -0,0 +1,16 @@
+package br.com.joelamalio.calendar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@Configuration
+public class ThymleafConfiguration {
+	
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
+	}
+	
+}
