d2d1f8d4a5a37a1f4d01785d2a6868e57f029f4c
Joel Amálio <joel.amalio@gmail.com>
2018-11-08 19:08:55 -0300

    Validação do campo data e alteração do componente do campo de status.

--- src/main/java/br/com/joelamalio/calendar/config/WebConfig.java
+++ src/main/java/br/com/joelamalio/calendar/config/WebConfig.java
@@ -1,7 +1,11 @@
 package br.com.joelamalio.calendar.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.joelamalio.calendar.config.format.LocalDateFormatter;
@@ -12,6 +16,23 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addFormatter(new LocalDateFormatter());
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource bundle = new ReloadableResourceBundleMessageSource();
+		bundle.setBasename("classpath:/messages");
+		bundle.setDefaultEncoding("UTF-8");
+		
+		return bundle;
+	}
+	
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+	    LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
+	    validatorFactoryBean.setValidationMessageSource(messageSource());
+	    
+	    return validatorFactoryBean;
 	}
 
 }
