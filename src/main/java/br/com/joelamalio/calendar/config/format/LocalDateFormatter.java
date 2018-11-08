21c4560a3fcf9bc56b583c7ba212cb8a305ea4d3
Joel Amálio <joel.amalio@gmail.com>
2018-11-08 19:07:14 -0300

    Alteração do formatador para obter o locale do objeto.

--- src/main/java/br/com/joelamalio/calendar/config/format/LocalDateFormatter.java
+++ src/main/java/br/com/joelamalio/calendar/config/format/LocalDateFormatter.java
@@ -3,6 +3,7 @@ package br.com.joelamalio.calendar.config.format;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Locale;
 
 import org.springframework.format.Formatter;
@@ -11,12 +12,12 @@ public class LocalDateFormatter implements Formatter<LocalDate> {
 
 	@Override
 	public LocalDate parse(String text, Locale locale) throws ParseException {
-		return LocalDate.parse(text, DateTimeFormatter.ISO_DATE);
+		return LocalDate.parse(text, DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
 	}
 
 	@Override
 	public String print(LocalDate object, Locale locale) {
-		return DateTimeFormatter.ISO_DATE.format(object);
+		return DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).format(object);
 	}
-
+	
 }
