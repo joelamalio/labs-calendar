080381bc42333f37e249460907fa3cb54230b6eb
Joel Amálio <joel.amalio@gmail.com>
2018-11-14 00:02:01 -0300

    Criação da tela de login.

---
+++ src/main/java/br/com/joelamalio/calendar/controller/SegurancaController.java
@@ -0,0 +1,14 @@
+package br.com.joelamalio.calendar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SegurancaController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+}
